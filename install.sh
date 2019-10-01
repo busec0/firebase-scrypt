08db8c7c9ea9533a4b1b807d1152f078ef6af854
Sergiu Bulzan <sergiu.bulzan@yardi.com>
2019-10-01 14:38:03 +0300

    makes install script to work on macos

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
-echo '  [x] Install SLL dev dependency'
-sudo apt install libssl-dev 
+echo '  [x] Install OpenSSL'
+brew install openssl
 
-echo '  [x] Install automake'
-sudo apt install automake
+echo '  [x] Install autoconf automake and libtool'
+brew install autoconf automake libtool
 
 echo '  [x] Clone firebase Scrypt repo'
 git clone https://github.com/firebase/scrypt.git
+
+if hash brew 2>/dev/null; then
+    opensslPath=$(brew --prefix openssl)
+    export CFLAGS="$CFLAGS -I$opensslPath/include"
+    export LDFLAGS="$LDFLAGS -L$opensslPath/lib"
+fi
 
 echo '  [x] Build Scrypt'
 cd scrypt/
